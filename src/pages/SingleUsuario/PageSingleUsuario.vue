cdba44665deb14e36675fc79a3df58236b4d6ed3
jhonatan <jhowfrombrazil@gmail.com>
2023-01-09 12:09:18 -0300

    singleTask

--- src/pages/SingleUsuario/PageSingleUsuario.vue
+++ src/pages/SingleUsuario/PageSingleUsuario.vue
@@ -5,7 +5,7 @@
     </div>
   </header>
   <div class="container relative">
-    <div class="flex items-start mt-[-255px] mb-32">
+    <div class="flex items-end mt-[-255px] mb-32">
       <div class="mr-24">
         <OAvatar
           :foto="avatar"
@@ -112,115 +112,13 @@
                     <p class="text-headline-3 pl-16">Task</p>
                     <p class="text-headline-3 pl-16">Responsável</p>
                     <p class="text-headline-3 pl-16">Urgência</p>
-                    <p class="text-headline-3 pl-16">Data de Criação</p>
-                    <p class="text-headline-3 pl-16">Data Desejada</p>
-                    <p class="text-headline-3 pl-16">Data Prevista</p>
+                    <!-- <p class="text-headline-3 pl-16">Data de Criação</p> -->
+                    <p class="text-headline-3 pl-16">Desejada</p>
+                    <p class="text-headline-3 pl-16">Prevista</p>
                   </div>
 
                   <ul>
-                    <li
-                      class="task-item bg-white border border-neutral-100/10 rounded-[3px] dark:border-white/10 overflow-hidden px-16 py-[7px]"
-                    >
-                      <div class="base-grid">
-                        <div class="flex justify-between items-center">
-                          <q-icon
-                            name="svguse:/icons.svg#icon_drag"
-                            size="22px"
-                          ></q-icon>
-                          <q-icon
-                            name="svguse:/icons.svg#icon_check_circle"
-                            size="22px"
-                          ></q-icon>
-                        </div>
-
-                        <div class="pl-16">
-                          <p class="text-paragraph-2 text-neutral-100">
-                            Design System + Telas HUB | Novadata
-                          </p>
-                          <p class="text-paragraph-3 text-neutral-70">
-                            Projeto: HUB
-                          </p>
-                        </div>
-                        <div class="pl-16 relative">
-                          <div class="flex items-center">
-                            <div class="relative w-40 h-40 mr-8">
-                              <AvatarSingle
-                                class="!w-40 !h-40"
-                                :index="0"
-                                side="left"
-                                :item="listAvatar[0]"
-                              />
-                            </div>
-                            <div>
-                              <p class="text-paragraph-2 text-neutral-100">
-                                Você
-                              </p>
-                              <p class="text-paragraph-3 text-neutral-70">
-                                Criado por: Bernardo
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="pl-16">
-                          <UrgenciaStatus text="Moderada" />
-                        </div>
-                        <div class="pl-16">
-                          <div class="flex items-center">
-                            <q-icon
-                              name="svguse:/icons.svg#icon_calendar_create"
-                              size="24px"
-                              class="mr-8"
-                            >
-                            </q-icon>
-                            <div>
-                              <p class="text-paragraph-2 text-neutral-100">
-                                09/12/2022
-                              </p>
-                              <p class="text-paragraph-3 text-neutral-70">
-                                Sexta
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="pl-16">
-                          <div class="flex items-center">
-                            <q-icon
-                              name="svguse:/icons.svg#icon_calendar_heart"
-                              size="24px"
-                              class="mr-8"
-                            >
-                            </q-icon>
-                            <div>
-                              <p class="text-paragraph-2 text-neutral-100">
-                                12/12/2022
-                              </p>
-                              <p class="text-paragraph-3 text-neutral-70">
-                                Terça
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="pl-16">
-                          <div class="flex items-center">
-                            <q-icon
-                              name="svguse:/icons.svg#icon_date_time"
-                              size="24px"
-                              class="mr-8 text-neutral-100/40"
-                            >
-                            </q-icon>
-                            <div>
-                              <p class="text-paragraph-2">12/12/2022</p>
-                              <p class="text-paragraph-3 text-neutral-70">
-                                Terça
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="pl-16">
-                          <TimerTask />
-                        </div>
-                      </div>
-                    </li>
+                    <TaskColaborador />
                   </ul>
                 </div>
               </div>
@@ -260,11 +158,12 @@ import OAvatar from 'src/components/Avatar/OAvatar.vue'
 import avatar from 'src/assets/image/gravatar.jpg'
 import KanbanItemEditableSelect from 'src/components/Kanban/KanbanItemEditableSelect.vue'
 import AvatarMultipleVue from 'src/components/Avatar/AvatarMultiple.vue'
-import AvatarSingle from 'src/components/Avatar/AvatarSingle.vue'
+
 import OAccordion from 'src/components/Accordion/OAccordion.vue'
 import OButton from 'src/components/Button/OButton.vue'
 import UrgenciaStatus from 'src/components/Urgencia/UrgenciaStatus.vue'
 import TimerTask from 'src/components/TimerTask/TimerTask.vue'
+import TaskColaborador from 'src/components/TaskColaborador/TaskColaborador.vue'
 
 const { usuarios } = inject('usuarios')
 
@@ -298,8 +197,6 @@ const listAvatar = ref([
 
 .base-grid
   display: grid
-  grid-template-columns: 70px 1fr 0.5fr 144px 144px 144px 144px 144px
+  grid-template-columns: minmax(55px, 65px)  minmax(200px, 1fr) minmax(170px, 204px) repeat(3, 100px) minmax(120px, 130px)
   align-items: center
-
-  .task-item
 </style>
