b5b4fef46d58c583fb653b06629d6ff84733016b
MarlonVictor <cmarlonvictor11@gmail.com>
2023-01-11 09:12:31 -0300

    Estatico da tabela recorrente feito

--- src/components/VisaoGeral/VisaoGeralTable.vue
+++ src/components/VisaoGeral/VisaoGeralTable.vue
@@ -1,5 +1,10 @@
 <template>
-  <OTable :rows="rows" :columns="columns" primary class="px-16 py-8">
+  <OTable
+    :rows="rows"
+    :columns="columns"
+    primary
+    class="px-16 py-8 visao-geral-table"
+  >
     <template v-slot:top-left>
       <OInput
         size="md"
@@ -10,9 +15,22 @@
       />
     </template>
 
+    <template v-slot:top-right>
+      <div class="flex gap-8 items-center">
+        <span class="text-paragraph-2 text-neutral-70">Mostrar</span>
+        <OSelect
+          class="w-72 h-40"
+          size="md"
+          v-model="linesPerPage"
+          :options="[10, 20, 50]"
+        />
+        <span class="text-paragraph-2 text-neutral-70">Linhas por página</span>
+      </div>
+    </template>
+
     <template v-slot:body="props">
       <q-tr :props="props">
-        <q-td key="projeto" :auto-width="false" class="!border-r">
+        <q-td key="projeto" :auto-width="false" class="!border-r w-full">
           {{ props.row.projeto }}
         </q-td>
 
@@ -37,6 +55,41 @@
             <p class="">{{ props.row.responsavel_atendimento?.nome }}</p>
           </div>
         </q-td>
+
+        <q-td
+          key="ultimo_acompanhamento"
+          :auto-width="false"
+          class="text-right !border-r"
+        >
+          {{ props.row.ultimo_acompanhamento }}
+        </q-td>
+
+        <q-td key="status" :auto-width="false" class="!border-r">
+          <o-badge size="lg" color="var(--alert-error)" square class="w-[8rem]">
+            <template v-slot:content>Atrasado</template>
+          </o-badge>
+        </q-td>
+
+        <q-td
+          key="chamado_pendentes"
+          :auto-width="false"
+          class="text-right !border-r w-96"
+        >
+          {{ props.row.chamado_pendentes }}
+        </q-td>
+
+        <q-td
+          key="tasks_pendentes"
+          :auto-width="false"
+          class="text-right !border-r w-96"
+        >
+          {{ props.row.tasks_pendentes }}
+        </q-td>
+
+        <q-td key="tasks_pendentes" class="text-center !px-6">
+          <OButton secondary icon="svguse:/icons.svg#icon_config" size="sm" />
+          <OButton secondary icon="search" size="sm" class="ml-6" />
+        </q-td>
       </q-tr>
     </template>
   </OTable>
@@ -47,8 +100,12 @@ import { ref } from 'vue'
 import OTable from 'src/components/Table/OTable.vue'
 import OInput from 'src/components/Input/OInput.vue'
 import AvatarSingle from 'src/components/Avatar/AvatarSingle.vue'
+import OBadge from 'src/components/Badge/OBadge.vue'
+import OButton from 'src/components/Button/OButton.vue'
+import OSelect from 'src/components/Select/OSelect.vue'
 
 const filter = ref('')
+const linesPerPage = ref(10)
 
 const columns = [
   {
@@ -120,9 +177,9 @@ const rows = [
       foto: 'https://avatars.githubusercontent.com/u/62356988?v=4',
     },
     ultimo_acompanhamento: '10/10/2022 - 17h 21m',
-    status: 87,
-    chamado_pendentes: '14%',
-    tasks_pendentes: '1%',
+    status: 'Finalizado',
+    chamado_pendentes: 10,
+    tasks_pendentes: 11,
     acoes: '1%',
   },
   {
@@ -136,12 +193,16 @@ const rows = [
       foto: 'https://avatars.githubusercontent.com/u/62356988?v=4',
     },
     ultimo_acompanhamento: '10/10/2022 - 17h 21m',
-    status: 87,
-    chamado_pendentes: '14%',
-    tasks_pendentes: '1%',
+    status: 'Atrasado',
+    chamado_pendentes: 10,
+    tasks_pendentes: 11,
     acoes: '1%',
   },
 ]
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.visao-geral-table
+  .q-chip :deep(.q-chip__content)
+    justify-content: center
+</style>
